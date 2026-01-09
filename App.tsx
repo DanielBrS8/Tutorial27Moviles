78cfec3ed4c1081e9e8055a61f523773131f792e
Daniel <danielbarrionuevo363@gmail.com>
2026-01-09 14:20:19 +0100

    primer commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import "./global.css"
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
 
-import './global.css';
+import clsx from "clsx";
+import { Toolbar } from "components/Toolbar";
+
+const styles = {
+  texto: "text-3xl font-bold"
+}
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <SafeAreaView className="flex-1">
+      <Toolbar/>
+      <View className="flex-1 bg-blue-100 justify-center items-center">
+        <Text className={clsx(styles.texto, "color-indigo-500")}>App</Text>
+        <Text className={clsx(styles.texto, "mt-3 color-[#FFA07A]")}>Nativewind</Text>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/Toolbar.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function Toolbar() {
+  return (
+    <View className='w-full h-14 flex-row items-center bg-blue-700'>
+    </View>
+  )
+}
